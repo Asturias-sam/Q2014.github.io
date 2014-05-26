e688b876ae0bad758acac13e10be180900c1d690
som <sompathaka@gmail.com>
2014-05-26 19:15:50 +0530

    Update keyboard_input_manager.js

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -69,7 +69,7 @@ KeyboardInputManager.prototype.listen = function () {
   });
 
   // Respond to button presses
-  this.bindButtonPress(".retry-button", this.restart);
+  
   this.bindButtonPress(".restart-button", this.restart);
   
 
